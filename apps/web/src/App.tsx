1b06340b1e478803d5eee98692728795f0a40614
syoudai0514 <syoudai0514@gmail.com>
2026-08-21 07:35:41 +0900

    Prevent blank screen after household setup

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { AuthProvider } from './app/AuthContext';
 import { AuthGate } from './app/AuthGate';
+import { AppErrorBoundary } from './components/AppErrorBoundary';
 
 function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <AuthGate />
-      </AuthProvider>
+      <AppErrorBoundary>
+        <AuthProvider>
+          <AuthGate />
+        </AuthProvider>
+      </AppErrorBoundary>
     </BrowserRouter>
   );
 }
